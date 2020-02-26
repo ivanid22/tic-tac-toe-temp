b159f8d030c01390cfafc3dadf6ed0f8e66dddd7
xtrmdarc <diegoreyesco@hotmail.com>
2020-02-26 14:45:05 -0500

    user interface completed

--- bin/main.rb
+++ bin/main.rb
@@ -9,11 +9,38 @@ class UserInterface
       end
       print "\n-----\n" if j < board.length-1
     end
-  end   
+    print "\n"
+  end
+  def self.ask_move(player)
+    puts "Enter move for player #{player[:id]}"
+    return gets.chomp
+  end
+
+  def self.repeat_match()
+    loop do
+      puts 'Play again? (y/n)'
+      input = (gets.chomp).downcase
+      return true if input == 'y'
+      return false if input == 'n'
+    end
+  end
+  def self.output_message(message)
+    puts message
+  end
 end
 
 puts 'Hello World!'
 
+player1 = {
+  id: 1
+}
+
 board = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
 
 UserInterface.display_board(board)
+
+move = UserInterface.ask_move(player1)
+
+
+answer = UserInterface.repeat_match
+puts answer
